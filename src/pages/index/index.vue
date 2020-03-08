982a54ec660ffe93421fb126e532e3b9124d8449
孙常宝 <1745523163@qq.com>
2020-03-08 10:47:17 +0800

    主页导航布局

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,7 +6,7 @@
       </view>
       <view class="content"></view>
     </view>
-    <swiper>
+    <swiper indicator-dots>
       <swiper-item>
         <image src="static/uploads/banner1.png"></image>
       </swiper-item>
@@ -17,37 +17,59 @@
         <image src="static/uploads/banner3.png"></image>
       </swiper-item>
     </swiper>
+    <view class="navs">
+      <navigator url="">
+        <image src="/static/"></image>
+      </navigator>
+      <navigator url="">
+        <image src=""></image>
+      </navigator>
+      <navigator url="">
+        <image src=""></image>
+      </navigator>
+      <navigator url="">
+        <image src=""></image>
+      </navigator>
+    </view>
   </view>
 </template>
 <script>
 export default {
   data() {
     return {
-      title: 'NIHAO'
-    }
-    },
-      onload(){
-    },
-    methods: {
-
-    }
-  }
+      title: "NIHAO"
+    };
+  },
+  onload() {},
+  methods: {}
+};
 </script>
 <style lang="less">
-.search{
-  .input-box{
-    background-color: #FF2D4A;
+.search {
+  .input-box {
+    background-color: #ff2d4a;
     padding: 20rpx 16rpx;
-    input{
+    input {
       background-color: #fff;
     }
   }
 }
-swiper{
+swiper {
   height: 340rpx;
-  image{
+  image {
     width: 750rpx;
     height: 340rpx;
+  }
+}
+.navs {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 30rpx;
+  background-color: pink;
+  align-items: center;
+  navigator {
+    width: 128rpx;
+    height: 140rpx;
   }
 }
 </style>
