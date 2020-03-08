390a4e472323a3699c60ccfea78924c8e4bb4124
孙常宝 <1745523163@qq.com>
2020-03-08 10:25:35 +0800

    主页-搜索栏布局

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <view>首页</view>
+  <view class="container">
+    <view class="search">
+      <view class="input-box">
+        <input type="text" />
+      </view>
+      <view class="content"></view>
+    </view>
+  </view>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'NIHAO'
+    },
+      onload(){
+    },
+    methods: {
+
+    }
+  }
+}
+</script>
+<style>
+</style>
