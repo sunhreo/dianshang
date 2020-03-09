0f7086df0f01c57fcbc9ec7fcd5d7646c410d320
孙常宝 <1745523163@qq.com>
2020-03-09 23:39:36 +0800

    首页完成

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,10 +1,28 @@
 <template>
   <view class="search" :class="{focused: isFocused}">
+    <!-- 搜索栏标签 -->
     <view class="input-box">
       <input :placeholder="placeholder" type="text" @focus="goSearch" />
       <text class="cancel" @click="handleCancel">取消</text>
     </view>
-    <view class="content" :style="{height:pageHeight,overflow:'hidden'}"></view>
+    <!-- 搜索的结果 -->
+    <view class="content">
+      <!-- 标题 -->
+      <div class="title">
+        搜索历史
+        <span class="clear"></span>
+      </div>
+      <!-- 搜索历史关键字 -->
+      <div class="history">
+        <navigator url>小米</navigator>
+        <navigator url>华为</navigator>
+        <navigator url>苹果</navigator>
+      </div>
+      <!-- 搜索结果 -->
+      <!-- <scroll-view scroll-y class="result">
+        <navigator url=''>冰箱</navigator>
+      </scroll-view>-->
+    </view>
   </view>
 </template>
 <script>
@@ -15,26 +33,19 @@ export default {
       placeholder: ''
     }
   },
-  onLoad() {
-    const { windowHeight } = uni.getSystemInfoSync()
-    console.log(windowHeight)
-  },
   methods: {
     goSearch() {
       const { windowHeight } = uni.getSystemInfoSync()
-      console.log("hello")
-      this.$emit("window-Height", {
-        height: uni.getSystemInfoSync().windowHeight
-      });
-      // 当输入获取焦点是,在父元素添加一个类名
+      // 将可视区高度传递到父组件
+      this.$emit('window-height', { height: windowHeight })
+      // 当输入框获取焦点时，在父元素添加一个类名 focused
       this.isFocused = true
       this.placeholder = '请输入想要的商品'
     },
     handleCancel() {
-      // 取消
-      this.$emit('search', {
-        pageHeight: 'auto'
-      })
+      // 点击取消按钮恢复原始状态
+      this.$emit('window-height', { height: 'auto' })
+      // 取消动作：恢复原始状态
       this.isFocused = false
       this.placeholder = ''
     }
@@ -52,22 +63,49 @@ export default {
     z-index: 9;
     background-color: #fff;
     display: none;
+    padding: 30rpx;
+    .title {
+      font-size: 27rpx;
+      line-height: 1;
+      color: #333;
+    }
+    .clear {
+      display: block;
+      width: 27rpx;
+      height: 27rpx;
+      float: right;
+      background-image: url(http://static.botue.com/ugo/images/clear.png);
+      background-size: cover;
+    }
+    .history {
+      padding-top: 30rpx;
+      navigator {
+        display: inline-block;
+        line-height: 1;
+        padding: 15rpx 20rpx 12rpx;
+        background-color: #ddd;
+        font-size: 24rpx;
+        margin-right: 20rpx;
+        margin-bottom: 15rpx;
+        color: #333;
+      }
+    }
   }
   .input-box {
     background-color: #ff2d4a;
     padding: 20rpx 16rpx;
     display: flex;
-    height: 60rpx;
     position: relative;
     input {
-      flex: 1;
+      background-color: #fff;
       padding-left: 55rpx;
       height: 60rpx;
-      background-color: #fff;
+      flex: 1;
     }
     .cancel {
       display: none;
     }
+
     &::before {
       content: "";
       display: none;
@@ -79,6 +117,7 @@ export default {
       background-image: url("http://static.botue.com/ugo/images/icon_search%402x.png");
       background-size: 32rpx;
     }
+
     &::after {
       content: "搜索";
       position: absolute;
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,86 +1,124 @@
 <template>
-  <view class="container" :style="{height:pageHeight,overflow:'hidden'}">
-    <search  @window-height='handleWindowHeight'/>
+  <view class="container" :style='{height: pageHeight, overflow: "hidden"}'>
+    <!-- 搜索栏 -->
+    <search @window-height="handleWindowHeight" />
+
+    <!-- 轮播图 -->
     <swiper indicator-dots>
       <swiper-item :key="item.goods_id" v-for="item in swiperData">
-        <image :src="item.image_src"/>
+        <image :src="item.image_src" />
       </swiper-item>
     </swiper>
+
+    <!-- 导航菜单 -->
     <view class="navs">
-      <navigator url="" :key="index" v-for="(item,index) in navsData">
-        <image :src="item.image_src  "></image>
-       </navigator>
-     
+      <navigator url :key="index" v-for="(item, index) in navsData">
+        <image :src="item.image_src" />
+      </navigator>
     </view>
+
+    <!-- 楼层商品 -->
     <view class="floors">
-      <view class="floor" :key="index" v-for="(item,index)in floorData">
+      <view class="floor" :key="index" v-for="(item, index) in floorData">
+        <!-- 标题 -->
         <view class="title">
-          <image :src="floor_title.image_src">
+          <image :src="item.floor_title.image_src" />
         </view>
+        <!-- 列表 -->
         <view class="items">
-          <navigator url="" :key="i" v-for="(img,i) in item.product_list">
-            <image src="img.image_src"/>
+          <navigator url :key="i" v-for="(img, i) in item.product_list">
+            <image :src="img.image_src" />
           </navigator>
-        
         </view>
       </view>
-   
     </view>
-      </view>
+
+    <!-- 回到顶部按钮 -->
+    <view class="goTop icon-top" @click="goTop" v-if="scrollTop > 100"></view>
+  </view>
 </template>
+
 <script>
 import search from '@/components/search'
 export default {
   data() {
     return {
-    pageHeight:'auto',
-    title: "NIHAO",
-    swiperData:[],
-    navsData:[],
-    floorData:[]
-    };
+      title: 'Nihao',
+      pageHeight: 'auto',
+      swiperData: [],
+      navsData: [],
+      floorData: [],
+      // 页面滚动的距离
+      scrollTop: 0
+    }
   },
-  components:{
+  components: {
     search
   },
-  onload() {
+  onPageScroll(e) {
+    // 监听页面的滚动，获取当前滚动的距离
+    this.scrollTop = e.scrollTop
+  },
+  onLoad() {
     this.querySwiperData()
     this.queryNavsData()
-    this.floorData()
+    this.queryFloorData()
   },
   methods: {
-    handleWindowHeight(data){
-      this.pageHeight=data.height +'px';
-      cons0ole.log('handleWindowHeight')
-    },
-    querySwiperData(){
-      wx.request({
-        url:'http://api-ugo-dev.iteima.net/api/public/v1/home/swiperdata',
-        success:(res)=>{
-          this.swiperData=res.data.message
-        }
+    goTop() {
+      // 控制页面回到顶部
+      uni.pageScrollTo({
+        scrollTop: 0
       })
     },
-    queryNavsData(){
-      wx.request({
-        url:'http://api-ugo-dev.iteima.net/api/public/v1/home/catitems',
-        success:(res)=>{
-          this.navsData=res.data.message
-        }
-      })
+    handleWindowHeight(data) {
+      this.pageHeight = data.height + 'px'
     },
-    queryFloorData(){
-      wx.request({
-        url:'http://api-ugo-dev.iteima.net/api/public/v1/home/FloorData',
-        success:(res)=>{
-          this.navsData=res.data.message
-        }
+    async querySwiperData() {
+      // 获取轮播图数据
+      const { message } = await this.$request({
+        path: 'home/swiperdata'
       })
+      this.swiperData = message
+
+      // wx.request({
+      // 	url: 'https://api-ugo-dev.itheima.net/api/public/v1/home/swiperdata',
+      // 	success: (res) => {
+      // 		this.swiperData = res.data.message
+      // 	}
+      // })
+    },
+    async queryNavsData() {
+      // 获取导航菜单数据
+      const { message } = await this.$request({
+        path: 'home/catitems'
+      })
+      this.navsData = message
+      // wx.request({
+      // 	url: 'https://api-ugo-dev.itheima.net/api/public/v1/home/catitems',
+      // 	success: (res) => {
+      // 		this.navsData = res.data.message
+      // 	}
+      // })
+    },
+    async queryFloorData() {
+      // 获取导航菜单数据
+      const { message } = await this.$request({
+        path: 'home/floordata'
+      })
+      this.floorData = message
+      // wx.request({
+      // 	url: 'https://api-ugo-dev.itheima.net/api/public/v1/home/floordata',
+      // 	success: (res) => {
+      // 		this.floorData = res.data.message
+      // 	}
+      // })
     }
   }
-};
+}
 </script>
-<style lang="less">
+
+<style lang='less'>
 swiper {
   height: 340rpx;
   image {
@@ -99,13 +137,14 @@ swiper {
     height: 140rpx;
   }
 }
+
 .floor {
   .title {
-    background-color: #f4f4f4;
     width: 750rpx;
     height: 60rpx;
     padding: 20rpx 0 0 10rpx;
     box-sizing: border-box;
+    background-color: #f4f4f4;
   }
   .items {
     padding: 20rpx 16rpx;
@@ -133,17 +172,33 @@ swiper {
       }
     }
   }
-}
-&:first-child {
-		.items {
-			navigator {
-				width: 232rpx;
+
+  &:first-child {
+    .items {
+      navigator {
+        width: 232rpx;
         height: 188rpx;
         &:first-child {
-        	width: 232rpx;
+          width: 232rpx;
           height: 386rpx;
         }
-			}
-		}
-	}
+      }
+    }
+  }
+}
+
+.goTop {
+  position: fixed;
+  bottom: 30rpx;
+  right: 30rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100rpx;
+  height: 100rpx;
+  font-size: 48rpx;
+  color: #666;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.8);
+}
 </style>
